f75b6253a77523d6d8b430a2f03bf450897cf8d1
Simulanis-Dev-PradeepKumar <pradeepkumar@simulanis.com>
2024-04-22 17:29:41 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to Simulanis');
+  res.send('UNO Product deployment task has work in progress');
 });
 const port = process.env.PORT ?? 4000;
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
