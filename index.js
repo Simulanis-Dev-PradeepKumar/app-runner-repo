10d4aecd868638517196aeebf540b6c6ccd89d08
Simulanis-Dev-PradeepKumar <pradeepkumar@simulanis.com>
2024-04-22 18:22:31 +0530

    Update message in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('UNO Product deployment task has work in progress');
+  res.send('UNO Product deployment task is work in progress');
 });
 const port = process.env.PORT ?? 4000;
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
